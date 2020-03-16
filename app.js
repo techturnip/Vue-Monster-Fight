97546d0fac005974f9a6da39f6743979660f46d3
Tyler Turnipseed <tyler@techturnip.us>
2020-03-16 16:04:37 -0400

    setup attack values for monster to player damage

--- app.js
+++ app.js
@@ -8,21 +8,29 @@ new Vue({
   methods: {
     startGame: function () {
       // Set boolean to true
-      this.gameIsRunning = true;
+      this.gameIsRunning = true
       // Reset health for fresh game
-      this.playerHealth = 100;
-      this.monsterHealth = 100;
+      this.playerHealth = 100
+      this.monsterHealth = 100
     },
     attack: function () {
+      // Player damage to monster
       // set max damage amount
-      let max = 10;
+      let max = 10
       // set min damage amount
-      let min = 3;
+      let min = 3
       // set damage to be a random amount between 3 and 10
       let damage = Math.max(Math.floor(Math.random() * max) + 1, min)
 
       // decrement monsterHealth by the damage inflicted
-      this.monsterHealth -= damage;
+      this.monsterHealth -= damage
+
+      // Monster damage to player
+      // TODO: refactor to be more DRY
+      max = 12
+      min = 5
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+      this.playerHealth -= damage
     },
     specialAttack: function () {},
     heal: function () {},
