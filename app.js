9082c3b5ae233b26997a54b5460551a1d76afe64
Tyler Turnipseed <tyler@techturnip.us>
2020-03-16 16:01:56 -0400

    create attack method and setup damage calc

--- app.js
+++ app.js
@@ -13,7 +13,17 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
-    attack: function () {},
+    attack: function () {
+      // set max damage amount
+      let max = 10;
+      // set min damage amount
+      let min = 3;
+      // set damage to be a random amount between 3 and 10
+      let damage = Math.max(Math.floor(Math.random() * max) + 1, min)
+
+      // decrement monsterHealth by the damage inflicted
+      this.monsterHealth -= damage;
+    },
     specialAttack: function () {},
     heal: function () {},
     giveUp: function () {}
