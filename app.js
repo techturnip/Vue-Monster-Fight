2c98d6d34f59d6ae23cd13def2ad7e6705ea1aaf
Tyler Turnipseed <tyler@techturnip.us>
2020-03-16 15:48:23 -0400

    start new game btn hooked up to even listener, calls startGame fn and successfully toggles controls

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false
+  },
+  methods: {
+    startGame: function () {
+      // Set boolean to true
+      this.gameIsRunning = true;
+      // Reset health for fresh game
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    }
   }
 })
